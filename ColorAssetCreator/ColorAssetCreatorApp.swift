e493c574393bd3e433e528191f1ce1c50b5ef331
JonathanChacon <tyflosaccessiblesoftware@gmail.com>
2025-05-16 06:07:27 +0200

    Initial Commit

---
+++ ColorAssetCreator/ColorAssetCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAssetCreatorApp.swift
+//  ColorAssetCreator
+//
+//  Created by Jonathan Chacón Barbero on 16/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ColorAssetCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
