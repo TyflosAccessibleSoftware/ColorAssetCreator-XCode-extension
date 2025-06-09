4b6adeb0e35af43fb07f56b5fbc1b913574ecc6a
JonathanChacon <tyflosaccessiblesoftware@gmail.com>
2025-06-09 06:44:01 +0200

    Blank remake of a MacOS app project

---
+++ ColorAssetCreator/ColorAssetCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAssetCreatorApp.swift
+//  ColorAssetCreator
+//
+//  Created by Jonathan Chacón Barbero on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ColorAssetCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
